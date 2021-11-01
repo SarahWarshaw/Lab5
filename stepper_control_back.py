3b236165a7bb3d6851ce5a9a031f59a4dd5b6d1e
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-01 18:45:53 +0000

    cgi try

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -4,32 +4,29 @@ from PCF8591 import PCF8591
 from Stepper import Stepper
 import RPi.GPIO as GPIO
 import time
+import json
 
 GPIO.setmode(GPIO.BCM)
-'''
-# Infinite loop reading data from file saved by cgi code
-while True:
-  with open('stepper_control.py','r') as f:
-    data = json.load(f)
 
-  value = float(data['slider'])
-
-if value == 400:
-  #run zero code
-else:
-  # determine if CW or CCW based on previous angle/ whether the angle needed to turn is greater than 180
-  #run angle code
-
-'''
-
-
-GPIO.setmode(GPIO.BCM)
 myStepper = Stepper(0x48)
-
+# Infinite loop reading data from file saved by cgi code
+prevAng = 0
 try:
-  print('here')
-  #myStepper.goAngle(360,1)
-  myStepper.zero()
+  while True:
+    with open('stepper_control.txt','r') as f:
+      data = json.load(f)
+
+    value = float(data['slider'])
+
+    if value == 400:
+      myStepper.zero()
+      #run zero code
+    else:
+      myStepper.goAngle(abs(value-prevAng),dir)
+      # determine if CW or CCW based on previous angle/ whether the angle needed to turn is greater than 180
+      #run angle code
+    prevAng = value
+
 except Exception as e: 
   print(e)
 GPIO.cleanup()
