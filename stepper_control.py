d8c522759cc6559aab4a20ff1d5c6e83f82e0f74
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-01 21:32:16 +0000

    cgi-bin

--- stepper_control.py
+++ stepper_control.py
@@ -2,16 +2,13 @@
 import cgi
 import json
 
-try:
-  print("Content-type: text/html\n\n")
-  data = cgi.FieldStorage()
-  selection = data.getValue('two_buttons')
-  data = {"two_buttons":selection}
-  with open('stepper_control.txt','w') as f:
-    json.dump(data,f)
+print("Content-type: text/html\n\n")
+data = cgi.FieldStorage()
+selection = data.getValue('two_buttons')
+data = {"two_buttons":selection}
+with open('stepper_control.txt','w') as f:
+  json.dump(data,f)
 
-except Exception as e: 
-  print(e)
 
 print("""
 <html>
@@ -23,7 +20,7 @@ print("""
 <h1>Choose an angle or zero the motor</h1>
 <font size="3" color = "black" face = "helvetica">
 <br>
-<form action = "stepper_control.py" method = "POST">
+<form action = "/cgi-bin/stepper_control.py" method = "POST">
   <input type ="range" name = "slider" min = "0" max="360" value="0"><br>
   <input type="submit" name="button" value = "Submit angle"><br><br>
   <input type="submit" name = "button" value = "Zero the motor">
