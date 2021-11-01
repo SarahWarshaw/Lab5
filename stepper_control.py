136cf61135e87993320911bc1e04757ac4be792e
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-01 19:02:07 +0000

    write file

--- stepper_control.py
+++ stepper_control.py
@@ -5,7 +5,7 @@ import json
 print("Content-type: text/html\n\n")
 data = cgi.FieldStorage()
 s1 = data.getvalue('slider')
-data = {'slider':s1}
+data = {"slider":s1}
 with open('stepper_control.txt','w') as f:
   json.dump(data,f)
 
@@ -18,11 +18,9 @@ print("""
 <br>
 <font size="3" color = "black" face = "helvetica">
 <br>
-<form action = "stepper_control.py" method = "POST">
+<form action = "/cgi-bin/stepper_control.py" method = "POST">
   <input type ="range" name = "slider" min = "0" max="360" value="0"><br>
   <input type="submit" value = "Submit angle"><br><br>
-  <input type = "hidden" name = "slider" value = "400">
-  <input type="submit" value = "Zero the motor">
 </form>
 </div>
 </body>
