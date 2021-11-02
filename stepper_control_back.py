8f6632d63d6a622a323ec3c8bcdd59ca6b9acede
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-02 13:15:02 +0000

    simple

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -11,31 +11,23 @@ GPIO.setmode(GPIO.BCM)
 myStepper = Stepper(0x48)
 # Infinite loop reading data from file saved by cgi code
 prevAng = 0
-try:
-  while True:
-    with open('stepper_control.txt','r') as f:
-      data = json.load(f)
-
-# duty cycle (1-100) is the value of 'slider' and ledPin(13,19, or 26) is the value of 'LED'
-    # selection = data['Buttons']
-    angle = int(data['slider'])
-      
+while True:
+  with open('stepper_control.txt','r') as f:
+    data = json.load(f)
+  dutyCycle = float(data['slider'])
+  ledPin = data['Buttons']
+    
 
 
-    #if selection == 'Zero Motor':
-      #myStepper.zero()
-      #run zero code
-    #else:
-    #  myStepper.goAngle(angle,dir)
-      # determine if CW or CCW based on previous angle/ whether the angle needed to turn is greater than 180
-      #run angle code
-    prevAng = angle
-    with open('stepper_control.txt','w') as f:
-      data = {"slider":0,"Buttons": "Change Angle"}
-      json.dump(data,f)
-      
-
-except Exception as e: 
-  print(angle)
-  print(e)
-GPIO.cleanup()
+  #if selection == 'Zero Motor':
+    #myStepper.zero()
+    #run zero code
+  #else:
+  #  myStepper.goAngle(angle,dir)
+    # determine if CW or CCW based on previous angle/ whether the angle needed to turn is greater than 180
+    #run angle code
+  # prevAng = angle
+  #with open('stepper_control.txt','w') as f:
+    #data = {"slider":0,"Buttons": "Change Angle"}
+    #json.dump(data,f)
+  GPIO.cleanup()
