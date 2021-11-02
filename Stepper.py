545d09e7bfa15a3a5f345e9f13cee311e66c1f9c
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-02 16:06:28 +0000

    photores

--- Stepper.py
+++ Stepper.py
@@ -35,8 +35,6 @@ class Stepper:
   def turnSteps(self,steps,dir):   
     for step in range(steps):
       self.halfstep(dir)
-    angle = self.myPCF8591.read(0)
-    print(angle)
 
   def goAngle(self, angle,dir):
     # move to a specified angle
