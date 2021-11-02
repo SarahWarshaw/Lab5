16eab0633ef1a0bb3e36202e7ab7183882fe9f8a
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-02 13:47:24 +0000

    try again

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+import RPi.GPIO as gpio
+import time
+import json
+
+# Set gpio pins as outputs with 100 Hz frequency
+gpio.setmode(gpio.BCM)
+gpio.setup(13, gpio.OUT)
+gpio.setup(19, gpio.OUT)
+gpio.setup(26, gpio.OUT)
+pwm1 = gpio.PWM(13, 100)
+pwm2 = gpio.PWM(19, 100)
+pwm3 = gpio.PWM(26, 100)
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+
+# Infinite loop reading data from file saved by cgi code
+while True:
+  with open('stepper_control.txt','r') as f:
+    data = json.load(f)
+
+# duty cycle (1-100) is the value of 'slider' and ledPin(13,19, or 26) is the value of 'LED'
+  dutyCycle = float(data['slider'])
+  ledPin = data['Buttons']
+
 #from PCF8591 import PCF8591
 #from Stepper import Stepper
 #import RPi.GPIO as GPIO
