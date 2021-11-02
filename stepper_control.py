f35b1b2780843cc2af6b922268f60ec6f4793e98
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-02 17:15:35 +0000

    second frame

--- stepper_control.py
+++ stepper_control.py
@@ -39,9 +39,11 @@ print('<input type ="range" name = "slider" min = "0" max="360" value="0"><br>')
 print("""
   <input type="submit" name = "Buttons" value = "Change Angle">
   <input type="submit" name ="Buttons" value = "Zero Motor">
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1539962/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=step
-  &api_key=3UQYF5FIM794CZYD"></iframe>
 </form>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1539962/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=step
+&api_key=3UQYF5FIM794CZYD"></iframe>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1539962/widgets/368555
+&api_key=3UQYF5FIM794CZYD"></iframe>
 </div>
 </body>
 </html>
