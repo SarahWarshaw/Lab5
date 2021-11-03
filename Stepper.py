f57cc30c6854df0b6638430f13e63be6fbcf536f
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-03 01:30:58 +0000

    225

--- Stepper.py
+++ Stepper.py
@@ -53,7 +53,7 @@ class Stepper:
     # lower number means brighter, ambient light is higher value
     photores = self.myPCF8591.read(0)
     print(photores)
-    while photores < 250: 
+    while photores < 225: 
       photores = self.myPCF8591.read(0)
       self.turnSteps(1,1)
       self.delay_us(100)
